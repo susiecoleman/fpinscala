ae2d249156e0ac8a6ae153e1915d495484f7948a
Susie Coleman <susie.coleman@guardian.co.uk>
2017-03-09 11:33:33 +0000

    Lists

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -1,5 +1,8 @@
 package fpinscala.datastructures
 
+import scala.annotation.tailrec
+
+
 sealed trait List[+A] // `List` data type, parameterized on a type, `A`
 case object Nil extends List[Nothing] // A `List` data constructor representing the empty list
 /* Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`,
@@ -31,18 +34,6 @@ object List { // `List` companion object. Contains functions for creating and wo
     case _ => 101
   }
 
-  def append[A](a1: List[A], a2: List[A]): List[A] =
-    a1 match {
-      case Nil => a2
-      case Cons(h,t) => Cons(h, append(t, a2))
-    }
-
-  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
-    as match {
-      case Nil => z
-      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
-    }
-
   def sum2(ns: List[Int]) =
     foldRight(ns, 0)((x,y) => x + y)
 
@@ -50,19 +41,116 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = ???
+  def tail[A](l: List[A]): List[A] = l match {
+    case Cons(h, t) => t
+    case _ => Nil
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  def setHead[A](l: List[A], newHead: A): List[A] = l match {
+    case Cons(h,t) => Cons(newHead, t)
+    case _ => Nil
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  def drop[A](l: List[A], n: Int): List[A] =
+    if (n > 0) {
+      l match {
+        case Cons(h, t) => drop(t, n-1)
+        case _ => Nil
+      }
+    } else {
+      l
+    }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  def dropWhile[A](l: List[A])(f: A => Boolean): List[A] = {
+    def dropWhileAcc(list: List[A], acc: List[A]): List[A] = {
+      list match {
+        case Cons(h,t) => if (f(h)) dropWhileAcc(t, Cons(h, acc)) else acc
+        case _ => acc
+      }
+    }
+    dropWhileAcc(l, Nil)
+  }
 
-  def init[A](l: List[A]): List[A] = ???
+  def init[A](l: List[A]): List[A] = {
+    l match {
+      case Cons(_, Nil) => Nil
+      case Cons(h, t) => Cons(h, init(t))
+      case _ => Nil
+    }
+  }
 
-  def length[A](l: List[A]): Int = ???
+  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = as match {
+    case Nil => z
+    case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+  }
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  def length[A](l: List[A]): Int = foldRight(l, 0)((_, acc) => acc + 1)
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = ???
+  def foldLeft[A,B](as: List[A], z: B)(f: (B, A) => B): B = {
+    @tailrec
+    def foldLeftAcc(l: List[A], acc: B): B = {
+      l match {
+        case Cons(h, t) => foldLeftAcc(t, f(acc, h))
+        case Nil => acc
+      }
+    }
+    foldLeftAcc(as, z)
+  }
+
+  def length2[A](l: List[A]): Int = foldLeft(l, 0)((acc, _) => acc + 1)
+
+  def reverse[A](l: List[A]) = foldLeft(l, List[A]())((acc, h) => Cons(h, acc))
+
+  def foldLeft2[A, B](as: List[A], z: B)(f: (B, A) => B): B =
+    foldRight(reverse(as), z)((x: A, y: B) => f(y, x))
+
+  def foldRight2[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+    foldLeft(reverse(as), z)((x: B, y: A) => f(y, x))
+
+  def append[A](l1: List[A], l2: List[A]): List[A] = {
+    foldRight(l1, l2)((h, list) => Cons(h, list))
+  }
+
+  def listConcat[A](lists: List[List[A]]): List[A] = {
+    foldLeft(lists, List[A]())((h, l) => append(h, l))
+  }
+
+  def addOneLists(l: List[Int]): List[Int] = {
+    foldRight(l, List[Int]())((item, acc) => Cons(item + 1, acc))
+  }
+
+  def doubleToString(l: List[Double]): List[String] = {
+    foldRight(l, List[String]())((item, acc) => Cons(item.toString, acc))
+  }
+
+  def map[A,B](l: List[A])(f: A => B): List[B] = foldRight(l, List[B]())((item, acc) => Cons(f(item), acc))
+}
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    val l1 = List(1, 4, 7)
+    val l2 = List(3,1,2)
+    val l3 = List(7,8,2,0)
+    val lists = List(l1,l2,l3)
+    val l4 = List(3.4, 5.6, 12.1)
+    assert(List.append(l1, l2) == List(1,4,7,3,1,2))
+    assert(List.tail(l1) == List(4,7))
+    assert(List.setHead(l1, 19) == List(19, 4,7))
+    assert(List.drop(l1, 0) == List(1, 4,7))
+    assert(List.drop(l1, 5) == List())
+    assert(List.drop(l1, 2) == List(7))
+    assert(List.dropWhile(l1)(_%2 != 0) == List(1))
+    assert(List.foldRight(l1, "List")((a,b) => a+b) == "147List")
+    assert(List.foldRight2(l1, "List")((a,b) => a+b) == "147List")
+    assert(List.foldLeft(l1, "List")((a,b) => a+b) == "List147")
+    assert(List.foldLeft2(l1, "List")((a,b) => a+b) == "List147")
+    assert(List.length(l1) == 3)
+    assert(List.length2(l1) == 3)
+    assert(List.reverse(l1) == List(7,4,1))
+    assert(List.listConcat(lists) == List(1,4,7,3,1,2,7,8,2,0))
+    assert(List.addOneLists(l1) == List(2,5,8))
+    assert(List.doubleToString(l4) == List("3.4", "5.6", "12.1"))
+    assert(List.map(l1)(_.toString + "**") == List("1**", "4**", "7**"))
+  }
 }
