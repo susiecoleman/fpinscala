751b282fde17c4d1a3f731788d37aec08ee43231
Susie Coleman <susie.coleman@guardian.co.uk>
2017-05-26 16:07:58 +0100

    Basic either exercises.

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -4,13 +4,25 @@ package fpinscala.errorhandling
 import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
 sealed trait Either[+E,+A] {
- def map[B](f: A => B): Either[E, B] = ???
+ def map[B](f: A => B): Either[E, B] = this match {
+   case Right(a) => Right(f(a))
+   case Left(b) => Left(b)
+ }
 
- def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+ def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+   case Right(a) => f(a)
+   case Left(b) => Left(b)
+ }
 
- def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+ def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+   case Right(a) => Right(a)
+   case Left(_) => b
+ }
 
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+ def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = this match {
+   case Right(a) => b.map(f(a,_))
+   case Left(l) => Left(l)
+ }
 }
 case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
@@ -34,4 +46,25 @@ object Either {
     try Right(a)
     catch { case e: Exception => Left(e) }
 
+}
+
+object MainEither {
+
+  def test(x: Int): Int = x + 3
+
+  def main(args: Array[String]): Unit = {
+    assert(Right(5).map(_ + 6) == Right(11))
+    assert(Left(5).map(test) == Left(5))
+
+    assert(Right("hello").flatMap(_ => Right("goodbye")) == Right("goodbye"))
+    assert(Left(5).flatMap(_ => Right("goodbye")) == Left(5))
+
+    assert(Left(5).orElse(Left(12)) == Left(12))
+    assert(Right("hi").orElse(Left(12)) == Right("hi"))
+
+    assert(Right("hello").map2(Right("goodbye"))((x,y) => s"$x $y") == Right("hello goodbye"))
+    assert(Left(5).map2(Right("goodbye"))((x,y) => s"$x $y") == Left(5))
+    assert(Left(5).map2(Left(10))((x,y) => s"$x $y") == Left(5))
+    assert(Right("hello").map2(Left(10))((x,y) => s"$x $y") == Left(10))
+  }
 }
